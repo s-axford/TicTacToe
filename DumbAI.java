34a96e7775396e67cb703dd15f8681c6137dbb8a
noahgallant-MUN <noahgallant@pei.sympatico.ca>
2018-01-14 19:16:11 -0330

    DumbAI Changes

--- DumbAI.java
+++ DumbAI.java
@@ -7,33 +7,37 @@
 import java.util.Random;
 
 public class DumbAI implements AI {
-    
+
     private Random random = new Random();
 
     /*
      * TBD: Create additional private members if useful.
      */
-    
+
     /**
      * Construct a DumbAI.
-     * 
+     *
      * @param aiIsX Indicates whether the AI player's piece is
      *              the 'X'.
      */
-    public DumbAI(boolean aiIsX)
-    {
+    public DumbAI(boolean aiIsX) {
 
     }
 
     public Move chooseMove(Board board) {
 
         //Finds random number
+        while (found == false and board.isFull() == false)
         int col = random.nextInt();
         int col = col % 3;
         int row = random.nextInt();
         int row = row % 3;
 
-        if (board.get(col, row))
-
+        if (board.get(col, row) == " ")
+        {
+            found = true;
+            Move move = new Move(row, col, ?)
+        }
+        return move
     }
 }
