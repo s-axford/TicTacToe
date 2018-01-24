0f5c321d58d0102e275b9c374262a424974306b2
noahgallant-MUN <noahgallant@pei.sympatico.ca>
2018-01-24 01:05:12 -0330

    Added Comments

--- src/com/tictactoe/ConsoleRunner.java
+++ src/com/tictactoe/ConsoleRunner.java
@@ -33,6 +33,8 @@ public class ConsoleRunner {
     public ConsoleRunner() {
         boolean done = false;
         String input = " ";
+
+        //Will repeat if player inputs invalid characters
        while (done == false){
             System.out.println("Do you want to play as X? (Y/N)");
             input = scanner.next();
@@ -50,39 +52,74 @@ public class ConsoleRunner {
 
         input = " ";
         done = false;
+
+        //Will repeat if player inputs invalid characters
         while (done == false) {
             System.out.print("Do you want a challenge? (Y/N)");
-            input = scanner.next();
-            if (input.matches("Y")) {
-                challenging = true;
+            input = scanner.next();     //Expecting 'Y' or 'N'
+            if (input.matches("Y")) {           //If the user wants a challenge
+                challenging = true;     //Sets to SmartAI
                 done = true;
-            } else if (input.matches("N")) {
-                challenging = false;
+            } else if (input.matches("N")) {    //If the user doesn't want a challenge
+                challenging = false;    //Sets to DumbAI
                 done = true;
             }
             else {
-                System.out.println("Not valid input");
+                System.out.println("Not valid input");  //User inputted an invalid character
             }
         }
-        game = new Game(playerIsX , challenging);
+        game = new Game(playerIsX , challenging);       //Creates a new game
     }
 
+
+    /*
+     *  One round is considered a single piece placemed by both the player and the AI
+     */
     private void oneRound(boolean playerIsX)
     {
-        Integer row;
-        Integer col;
-        Board board = game.getBoard();
+        Integer row;        //Stores the user inputted row
+        Integer col;        //Stores the user inputted col
+        Board board = game.getBoard();  //Creates a new board in the current state
 
+        if (playerIsX) {
+
+            //Places the users piece
             do {
+                //Prompts the player to choose a move
+                System.out.println ("\n\n\n\n\n\n\n\nMake Your Move");
+                System.out.println(board.toString());
                 System.out.println("What row?");
-                row = scanner.nextInt() - 1;
+                row = scanner.nextInt() - 1;            //Users row selection
                 System.out.println("What col?");
-                col = scanner.nextInt() - 1;
-            }while (game.placePlayerPiece(row,col) == false);
-            if(game.getStatus() == GameStatus.IN_PROGRESS) {
+                col = scanner.nextInt() - 1;            //Users col selection
+            } while (game.placePlayerPiece(row, col) == false); //Will continue prompting the user if invalid input is entered
+
+            //Places the AI piece
+            if (game.getStatus() == GameStatus.IN_PROGRESS) {
                 game.aiPlacePiece();
             }
+
+        } else {
+
+            //Places the AI piece first
+            if (game.getStatus() == GameStatus.IN_PROGRESS) {
+                game.aiPlacePiece();
+            }
+
+            //Places the player piece second
+            if (game.getStatus() == GameStatus.IN_PROGRESS) {
+                do {
+                    //Prompts the player to choose a move
+                    System.out.println ("\n\n\n\n\n\n\n\nMake Your Move");
+                    System.out.println(board.toString());
+                    System.out.println("What row?");
+                    row = scanner.nextInt() - 1;
+                    System.out.println("What col?");
+                    col = scanner.nextInt() - 1;
+                } while (game.placePlayerPiece(row, col) == false);         //Will repeat if incorrect input is entered by the user
+            }
         }
+    }
 
 
     /*
@@ -92,34 +129,33 @@ public class ConsoleRunner {
     public void mainLoop() {
 
         //Creates a new game
+        Board board;
 
+        //Calls another round until the game status changes
         do
         {
-            //print board before round
-            Board board = game.getBoard();
-            System.out.println ("\n\n\n\n\n\n\n\nMake Your Move");
-            System.out.println(board.toString());
-            this.oneRound(playerIsX);
-        } while (game.getStatus() == GameStatus.IN_PROGRESS);
-        System.out.println("Game Over\n");
+            this.oneRound(playerIsX);       //Calls the next round of the game
+        } while (game.getStatus() == GameStatus.IN_PROGRESS);       //Will repeat until someone has one, or the board is full
+
+        //Continues below when the game is finished
+        board = game.getBoard();
+        System.out.println("\n\n\n\n\n\n\n\n\nGame Over\n");
+        System.out.println(board.toString());
+
+        //Checks if 'O' has won
         if (game.getStatus() == GameStatus.O_WON) {
-            System.out.println("O wins");
+            System.out.println("'O' wins");
         }
+
+        //Checks if 'X' has won
         if (game.getStatus() == GameStatus.X_WON) {
-            System.out.println("X wins");
+            System.out.println("'X' wins");
         }
+
+        //Check if there has been a draw
         if (game.getStatus() == GameStatus.DRAW) {
             System.out.println("Draw");
         }
 
-
-	/* TBD
-         *
-         * Use the 'nextInt' method of Scanner class to read user responses as
-         * integers.
-         *
-         * There is enough work to do here that you may want to introduce
-         * private methods (i.e. helper methods).
-         */
     }
 }
